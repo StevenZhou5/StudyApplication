192a4bd08746b792e7dda574668d4893b0d3d885
zhouzhenwu <zhouzhenwu@xiaomi.com>
2018-11-07 14:15:52 +0800

    日常代码更新

--- ZhouZhenWuAllDemoApplication/app/src/main/java/study/zhouzhenwu/com/mydemo/android/activity/AirbnbLottieAnimationTestActivity.java
+++ ZhouZhenWuAllDemoApplication/app/src/main/java/study/zhouzhenwu/com/mydemo/android/activity/AirbnbLottieAnimationTestActivity.java
@@ -7,12 +7,14 @@ import com.airbnb.lottie.LottieAnimationView;
 
 import study.zhouzhenwu.com.mydemo.R;
 import study.zhouzhenwu.com.mydemo.common.activity.BaseActivity;
+import study.zhouzhenwu.com.mydemo.common.widgets.PercentTextView;
 
 /**
  * Created by Steven on 2018/7/4.
  */
 
 public class AirbnbLottieAnimationTestActivity extends BaseActivity {
+    PercentTextView mPercentTextView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +25,15 @@ public class AirbnbLottieAnimationTestActivity extends BaseActivity {
         animationView.setAnimation("lottiefiles.com - Emoji Wink.json");
 //        animationView.setAnimation("City.json");
         animationView.loop(true);
+
+
+        mPercentTextView = (PercentTextView) findViewById(R.id.percent_tv);
+
+        mPercentTextView.setOnClickListener(v -> {
+            double percent = Math.random();
+            mPercentTextView.setPercent(percent);
+            mPercentTextView.setText((int) (percent * 100) + "%");
+
+        });
     }
 }
